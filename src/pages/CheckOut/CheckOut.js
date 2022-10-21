6e8ae32ccd90349af3b0cc258ac2de69fe97cee8
tam chi <chitam210893@gmail.com>
2022-10-21 20:52:47 +0700

    update

--- src/pages/CheckOut/CheckOut.js
+++ src/pages/CheckOut/CheckOut.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { layDanhSachPhongVe } from "../../redux/slice/checkoutSlice";
-import style from "./CheckOut.module.css";
+import { datVe, layDanhSachPhongVe } from "../../redux/slice/checkoutSlice";
+import "../../assets/css/CheckOut.css";
+import { CloseOutlined } from "@ant-design/icons";
 
 export default function CheckOut() {
   const { user } = useSelector((state) => state.userSlice);
@@ -13,26 +14,57 @@ export default function CheckOut() {
     dispatch(layDanhSachPhongVe(id));
   }, []);
 
-  const { danhSachPhongVe } = useSelector((state) => state.checkoutSlice);
-  console.log("danhSachPhongVe: ", danhSachPhongVe);
+  const { danhSachPhongVe, danhSachGheDangDat } = useSelector(
+    (state) => state.checkoutSlice
+  );
+  // console.log("danhSachPhongVe: ", danhSachPhongVe);
 
   const { thongTinPhim, danhSachGhe } = danhSachPhongVe;
+
+  const renderSeats = () => {
+    return danhSachGhe?.map((ghe, index) => {
+      let classGheVip = ghe.loaiGhe === "Vip" ? "gheVip" : "";
+      let classGheDaDat = ghe.daDat === true ? "gheDaDat" : "";
+
+      let indexGheDD = danhSachGheDangDat.findIndex(
+        (gheDD) => gheDD.maGhe === ghe.maGhe
+      );
+      return (
+        <Fragment key={index}>
+          <button
+            className={`ghe ${classGheVip} ${classGheDaDat} text-center`}
+            disabled={ghe.daDat}
+            onClick={() => {
+              dispatch(datVe(ghe));
+            }}
+          >
+            {ghe.daDat ? <CloseOutlined className="mb-1" /> : ghe.stt}
+          </button>
+          {(index + 1) % 16 === 0 ? <br /> : ""}
+        </Fragment>
+      );
+    });
+  };
 
   return (
     <div className="min-h-screen mt-5">
       <div className="grid grid-cols-12">
         <div className="col-span-9">
-          <div className="flex justify-center mt-5">
-            <div className=""></div>
-            <div className={`${style["trapezoid"]}`}>Màn hình</div>
+          <div className="flex flex-col items-center mt-5">
+            <div className="trapezoid">Màn hình</div>
+            <div>{renderSeats()}</div>
           </div>
         </div>
-        <div className="col-span-3 min-h-screen">
+        <div className="col-span-3 max-h-full">
           <h3 className="text-center text-2xl text-green-400">0 đ</h3>
           <hr />
           <h3 className="text-2xl font-bold">{thongTinPhim?.tenPhim}</h3>
-          <p>Địa chỉ: {thongTinPhim?.diaChi}</p>
-          <p>Ngày chiếu: {thongTinPhim?.ngayChieu}</p>
+          <p>
+            Địa chỉ: {thongTinPhim?.tenCumRap} - {thongTinPhim?.tenRap}
+          </p>
+          <p>
+            Ngày chiếu: {thongTinPhim?.ngayChieu} - {thongTinPhim?.gioChieu}
+          </p>
           <hr />
           <div className="flex flex-row my-5">
             <div className="w-4/5">
@@ -55,11 +87,8 @@ export default function CheckOut() {
             {user.soDT}
           </div>
           <hr />
-          <div
-            className="mb-0 flex flex-col justify-end items-center"
-            style={{ minHeight: "30vh" }}
-          >
-            <div className="bg-green-500 text-white w-full text-2xl text-center py-3 font-bold">
+          <div className="mt-10 w-11/12 mx-auto">
+            <div className="bg-green-500 text-white w-full text-2xl text-center py-3 font-bold cursor-pointer">
               ĐẶT VÉ
             </div>
           </div>
--- src/redux/slice/checkoutSlice.js
+++ src/redux/slice/checkoutSlice.js
@@ -3,12 +3,32 @@ import { checkoutService } from "../../services/checkoutService";
 
 const initialState = {
   danhSachPhongVe: {},
+  danhSachGheDangDat: [],
 };
 
 const checkoutSlice = createSlice({
   name: "checkoutSlice",
   initialState,
-  reducers: {},
+  reducers: {
+    datVe: (state, action) => {
+      // console.log(action.payload);
+      //cập nhật danh sách ghế đang đặt
+
+      let danhSachGheCapNhat = [...state.danhSachGheDangDat];
+      let index = danhSachGheCapNhat.findIndex(
+        (gheDD) => gheDD.maGhe === action.payload.maGhe
+      );
+
+      if (index != -1) {
+        //tìm thấy ghế đã được click
+        danhSachGheCapNhat.splice(index, 1);
+      } else {
+        danhSachGheCapNhat.push(action.payload);
+      }
+
+      state.danhSachGheDangDat = danhSachGheCapNhat;
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(layDanhSachPhongVe.fulfilled, (state, action) => {
       state.danhSachPhongVe = action.payload;
@@ -27,5 +47,7 @@ export const layDanhSachPhongVe = createAsyncThunk(
     }
   }
 );
+
+export const { datVe } = checkoutSlice.actions;
 
 export default checkoutSlice.reducer;

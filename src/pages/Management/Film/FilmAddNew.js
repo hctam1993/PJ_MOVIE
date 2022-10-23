288d009714b4cbad643e28679fca815ed89dd676
hctam1993 <yukito239@gmail.com>
2022-10-23 21:04:51 +0700

    update

--- src/pages/Management/Film/FilmAddNew.js
+++ src/pages/Management/Film/FilmAddNew.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import {
   Button,
-  Cascader,
   DatePicker,
   Form,
   Input,
@@ -9,11 +8,37 @@ import {
   Radio,
   Select,
   Switch,
-  TreeSelect,
 } from "antd";
+import { useFormik } from "formik";
+import moment from "moment";
 
 export default function FilmAddNew() {
   const [componentSize, setComponentSize] = useState("default");
+
+  const formik = useFormik({
+    initialValues: {
+      tenPhim: "",
+      trailer: "",
+      moTa: "",
+      ngayKhoiChieu: "",
+      dangChieu: false,
+      sapChieu: false,
+      hot: false,
+      danhGia: 0,
+      hinhAnh: {},
+    },
+    onSubmit: (values) => {
+      console.log("values: ", values);
+    },
+  });
+
+  const handleChangeDatePicker = (value) => {
+    let ngayKhoiChieu = moment(value).format("DD/MM/YYYY");
+    // console.log("ngayKhoiChieu: ", ngayKhoiChieu);
+
+    formik.setFieldValue("ngayKhoiChieu", ngayKhoiChieu);
+  };
+
   const onFormLayoutChange = ({ size }) => {
     setComponentSize(size);
   };
@@ -33,6 +58,7 @@ export default function FilmAddNew() {
         }}
         onValuesChange={onFormLayoutChange}
         size={componentSize}
+        onSubmitCapture={formik.handleSubmit}
       >
         <Form.Item label="Form Size" name="size">
           <Radio.Group>
@@ -42,32 +68,32 @@ export default function FilmAddNew() {
           </Radio.Group>
         </Form.Item>
         <Form.Item label="Tên phim">
-          <Input name="tenPhim" />
+          <Input name="tenPhim" onChange={formik.handleChange} />
         </Form.Item>
-        <Form.Item label="Trailer">
+        <Form.Item label="Trailer" onChange={formik.handleChange}>
           <Input name="trailer" />
         </Form.Item>
-        <Form.Item label="Mô tả">
+        <Form.Item label="Mô tả" onChange={formik.handleChange}>
           <Input name="moTa" />
         </Form.Item>
-        <Form.Item label="Select">
-          <Select>
-            <Select.Option value="demo">Demo</Select.Option>
-          </Select>
-        </Form.Item>
-
         <Form.Item label="Ngày khởi chiếu">
-          <DatePicker />
+          <DatePicker
+            format={"DD/MM/YYYY"}
+            onChange={(value) => {
+              handleChangeDatePicker(value);
+            }}
+          />
         </Form.Item>
 
         <Form.Item label="Đang chiếu" valuePropName="checked">
-          <Switch />
+          <Switch name="dangChieu" />
+          {/* lamf toiws dddaay */}
         </Form.Item>
         <Form.Item label="Sắp chiếu" valuePropName="checked">
-          <Switch />
+          <Switch name="sapChieu" />
         </Form.Item>
         <Form.Item label="Hot" valuePropName="checked">
-          <Switch />
+          <Switch name="hot" />
         </Form.Item>
         <Form.Item label="Số sao">
           <InputNumber />
@@ -75,8 +101,10 @@ export default function FilmAddNew() {
         <Form.Item label="Hình ảnh:">
           <input type="file"></input>
         </Form.Item>
-        <Form.Item label="Button">
-          <Button>Button</Button>
+        <Form.Item label="Tác vụ">
+          <Button type="primary" htmlType="submit" className="uppercase">
+            Thêm phim
+          </Button>
         </Form.Item>
       </Form>
     </div>
